0938e74d71542fe2f595db5a534146d0db85ff14
kishan <harikishan4000@gmail.com>
2023-06-29 03:46:02 +0530

    FrontEnd - Node generation

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ const root=document.querySelector(".gridItem10");
 root.classList.add("root");
 
 const testD=document.querySelector(".gridItem100");
-testD.classList.add("decision");
+// testD.classList.add("decision");
 
 const testC=document.querySelector(".gridItem88");
-testC.classList.add("cost");
+// testC.classList.add("cost");
 
 const testP=document.querySelector(".gridItem34");
-testP.classList.add("path");
+// testP.classList.add("path");
 
 
 
@@ -26,6 +26,9 @@ testP.classList.add("path");
 
 root.innerHTML= data[0].Id;
 let numOfNodesAtLevel=[];
+let col=[];
+let colDist=[];
+
 
 let maxLevel=0;
 
@@ -36,25 +39,49 @@ for(let i=0;i<data.length;i++){
     }
 }
 
+//initialize array values to 0
 for(let i=0;i<=maxLevel;i++){
     numOfNodesAtLevel[i]=0;
+    col[i]=0;
+    colDist[i]=0;
 }
 
+// count number of nodes in each level
 for(let i=0;i<data.length;i++){
     numOfNodesAtLevel[data[i].Level]++;
 
 }
+for(let i=0;i<=maxLevel;i++){
+    col[i]=Math.floor(21/(numOfNodesAtLevel[i]+1));
+    console.log(col[i])
+}
 
-    console.log(numOfNodesAtLevel);
+    // console.log(numOfNodesAtLevel);
 
-
-let row=Math.ceil(12/maxLevel);
-console.log(row);
+//row dist between levels
+let rowInc=Math.ceil(12/maxLevel-1);
+// console.log(rowInc);
 
 for(let i=0;i<data.length;i++){
+    let row=rowInc*data[i].Level*21;
+    colDist[data[i].Level]+=col[data[i].Level];
+    // console.log(row+col[data[i].Level])
+    // console.log(col[i])
+    // console.log(row)
+
 
     if(data[i].Type=="Decision"){
-        
+        let temp=document.querySelector(".gridItem"+String(Math.floor(row+colDist[data[i].Level])));
+        temp.classList.add("decision");
+        temp.innerHTML= data[i].Id;
 
+        
+    }
+    else if(data[i].Type=="Cost"){
+        let temp2=document.querySelector(".gridItem"+String(Math.floor(row+colDist[data[i].Level])));
+        temp2.classList.add("cost");
+        temp2.innerHTML= data[i].Id;
+        temp2.innerHTML= data[i].Name;
+        
     }
 }
